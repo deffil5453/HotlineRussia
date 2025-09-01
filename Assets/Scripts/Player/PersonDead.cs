4906d71a8058d0b7d46446ea73240adf81b33c3b
Gigabyte Дядя Витя <nozdrin_vitya@mail.ru>
2025-09-01 15:37:47 +0300

    ReworkSprite

--- Assets/Scripts/Player/PersonDead.cs
+++ Assets/Scripts/Player/PersonDead.cs
@@ -5,6 +5,15 @@ public class PersonDead : MonoBehaviour
     [SerializeField] private Sprite[] _deadSpriteRenderes;
     [SerializeField] private SpriteRenderer _currentSpriteRenderes;
 
+    private Rigidbody2D _currentRigidbody;
+    private Collider2D _currentCollider;
+
+    private void Awake()
+    {
+        _currentRigidbody = GetComponent<Rigidbody2D>();
+        _currentCollider = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bullet"))
@@ -18,7 +27,7 @@ public class PersonDead : MonoBehaviour
     {
         GetComponentInChildren<Animator>().enabled = false;
         int deadNumberSprite = Random.Range(0, _deadSpriteRenderes.Length);
-        
+        _currentCollider.enabled = false;
         _currentSpriteRenderes.sprite = _deadSpriteRenderes[deadNumberSprite];
     }
 }
